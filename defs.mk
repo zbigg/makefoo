ab5e28867388b2ccf26dbbc9947458640a15fe3f
z.zagorski <z.zagorski@gmail.com>
2012-04-04 05:01:33 +0000

    new feature: static/dynamic library which can be compiled from same component (see tests/autoconf_project LIBBAR

--- defs.mk
+++ defs.mk
@@ -46,15 +46,8 @@ define common_defs
 
 $(1)_builddir := $(top_builddir)/$$($(1)_DIR)
 $(1)_srcdir   := $(top_srcdir)/$$($(1)_DIR)
-
-# object files are keps in builddir/.target_type
-# as one component can be built in 
-# few ways
-ifeq ($(2),SHARED_LIBRARY)
-$(1)_objdir := $$($(1)_builddir)/.shobj
-else
 $(1)_objdir := $$($(1)_builddir)/.obj
-endif
+
 
 endef
 
