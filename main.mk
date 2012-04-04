9fe08b769c7f173854b22e54293113fa291a05d2
z.zagorski <z.zagorski@gmail.com>
2012-04-04 05:45:06 +0000

    create top_builddir for before running configure.sh if needed

--- main.mk
+++ main.mk
@@ -12,11 +12,13 @@ show:
 	@echo $($(NAME))
 
 $(top_builddir)/makefoo_configured_defs.mk: $(MAKEFOO)/configure.sh
+	@mkdir -p $(top_builddir)
 	MAKEFOO=$(MAKEFOO) $(MAKEFOO)/configure.sh > $@
 
 ifndef MAKEFOO_USE_AUTOCONF
 
 configure:
+	@mkdir -p $(top_builddir)
 	@rm $(top_builddir)/makefoo_configured_defs.mk
 	@$(MAKE) $(top_builddir)/makefoo_configured_defs.mk
 	@cat $(top_builddir)/makefoo_configured_defs.mk
