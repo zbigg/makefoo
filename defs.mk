992bfdab2284074ba13ceefc8de5eb99146a619a
z.zagorski <z.zagorski@gmail.com>
2012-04-19 23:06:25 +0000

    (cont) executable/library name is deduced from component name if xxx_NAME is not defined

--- defs.mk
+++ defs.mk
@@ -53,7 +53,7 @@ define common_defs
 $(1)_builddir := $(top_builddir)/$$($(1)_DIR)
 $(1)_srcdir   := $(top_srcdir)/$$($(1)_DIR)
 $(1)_objdir := $$($(1)_builddir)/.obj
-
+$(1)_name := $$(if $$($(1)_NAME),$$($(1)_NAME),$(1))
 
 endef
 
