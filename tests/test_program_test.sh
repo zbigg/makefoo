bbf87fa7a453b26e1493aba1ac6e48c8e158f15f
z.zagorski <z.zagorski@gmail.com>
2012-06-15 15:24:23 +0000

    veeery basic test for test_program feature added

---
+++ tests/test_program_test.sh
@@ -0,0 +1,26 @@
+. ./testlib.sh
+
+rm -rf test_program_test
+mkdir test_program_test
+cd test_program_test
+
+[ -f ../autoconf_project/configure ] || ( cd ../autoconf_project ; autoconf ; )
+
+invoke_test ../autoconf_project/configure --with-makefoo-dir=../..
+{
+    invoke_make test
+    assert_grep "this is bar_test, hello" stdout
+    assert_exists ./libbar/libbar_test_program
+    
+    invoke_make clean
+    
+    invoke_make COVERAGE=1 coverage-test-report
+    assert_grep "this is bar_test, hello" stdout
+    assert_exists ./libbar/libbar_test_program
+    
+    assert_exists ./coverage-test-report/index.html
+}
+cd ..
+
+rm -rf test_program_test
+
