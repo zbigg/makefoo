56783e161274851189cf4d2dc8b5069ec55032cf
z.zagorski <z.zagorski@gmail.com>
2012-11-08 18:39:44 +0000

    tests infra: added skip test support

--- tests/test.sh
+++ tests/test.sh
@@ -8,24 +8,31 @@ for test_script in *_test.sh ; do
     test_name=`echo $test_script | sed -e 'sX_test.shXX'`
     r=0
     (
-        echo "---- test $test_name"
+        echo "---- $test_name -- start"
         ./${test_script} 2>&1
         r=$?
         if [ "$r" = "0" ] ; then
-            echo "---- > success"
+            if egrep -q -- "^$test_name.*: skipped" $test_name.log ; then
+                echo "---- $test_name --> skipped"
+            else
+                echo "---- $test_name --> success"
+            fi
         else
-            echo "---- > failure, see $test_name.log"
+            echo "---- $test_name --> failure, see $test_name.log"
         fi
     ) | tee $test_name.log
-    if grep -qF -- "---- > failure" $test_name.log ; then
+    if grep -qF -- "---- $test_name --> failure" $test_name.log ; then
         overall_result=1
     fi
 done
 
 for test_script in *_test.sh ; do
     test_name=`echo $test_script | sed -e 'sX_test.shXX'`
-    if grep -qF -- "---- > failure" $test_name.log ; then
+    if grep -qF -- "---- $test_name --> failure" $test_name.log ; then
         echo "test '$test_name' failed"
+    fi
+    if grep -qF -- "---- $test_name --> skipped" $test_name.log ; then
+        echo "test '$test_name' skipped"
     fi
 done
 
