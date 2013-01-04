6da72351bec51216b0d553f009a9351d9cef1f21
z.zagorski <z.zagorski@gmail.com>
2013-01-04 19:54:57 +0000

    amalgamation added

---
+++ amalgamation.mk
@@ -0,0 +1,19 @@
+#
+# unity.mk
+#
+# unity build and/or amalgamation build is useful
+# when it is good to merge all source files of some components
+# and compile them as one step
+# (this requires well organized and idempotent headers)
+# 
+# unity-build described here:
+#  - http://cheind.wordpress.com/2009/12/10/reducing-compilation-time-unity-builds/
+# amalgamation concept name follows sqlite-amalgamation concept
+#
+# the actual implementation in unity.pre.mk as
+# it modifies definitions used
+# by native module
+#
+
+# jedit: :tabSize=8:mode=makefile:
+
