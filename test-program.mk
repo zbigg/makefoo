85c39deaf4d682555ce189e5476b8a2e0b881e37
z.zagorski <z.zagorski@gmail.com>
2014-01-10 22:55:50 +0000

    test-program: fix test_execuable rule, allow TEST_RUNNER to be specified, test can be execute under gdb/valgrind/wine/...

--- test-program.mk
+++ test-program.mk
@@ -30,14 +30,14 @@ define test_program_template
 $(1)_testdir := $$($(1)_builddir)/testdir
 $(1)_deps_build_dirs=$$(sort $$(foreach dep,$$($(1)_LINK_DEPS),$$(abspath $$($$(dep)_builddir))))
 $(1)_LD_LIBRARY_PATH=$$(subst $$(makefoo.space),:,$$($(1)_deps_build_dirs)):$$($(LD_LIBRARY_PATH_ENV_NAME))
-$(1)_test_executable = $$(realpath $$($(1)_builddir)/$$($(1)_name))
+$(1)_test_executable = $$(realpath $$($(1)_program_output))
 $(1)_tested_component_builddir = $$($($(1)_TESTED_COMPONENT)_builddir)
 $(1)_tested_component_sources = $$(realpath $$($($(1)_TESTED_COMPONENT)_sources_rel))
 
 $(1)-test: $$($(1)_outputs)
 	@mkdir -p $$($(1)_testdir)
 	$(COMMENT) "[$(1)] running test program: $$($(1)_test_executable)"
-	$(EXEC) cd $$($(1)_testdir) && $(LD_LIBRARY_PATH_ENV_NAME)="$$($(1)_LD_LIBRARY_PATH)" $$($(1)_test_executable) $$($(1)_TEST_INVOKE_ARGS)
+	$(EXEC) cd $$($(1)_testdir) && $(LD_LIBRARY_PATH_ENV_NAME)="$$($(1)_LD_LIBRARY_PATH)" $$(TEST_RUNNER) $$($(1)_test_executable) $$($(1)_TEST_INVOKE_ARGS)
 
 $(1)_coverage_test_result := $$($(1)_builddir)/coverage-test-result.lcov
 $(1)-coverage-test: $$($(1)_outputs)
