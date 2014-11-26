caa38a3fb7d3e90ce240817c2e5e453b89cd0d60
z.zagorski <z.zagorski@gmail.com>
2014-11-26 11:05:45 +0000

    native: TOOLSET=clang is equal to gcc for now

--- native.mk
+++ native.mk
@@ -1,8 +1,15 @@
 #ifndef TOOLSET
 #$(error TOOLSET not declared)
 #endif
+
+ifeq ($(TOOLSET),clang)
+TOOLSET_impl=gcc
+else
+TOOLSET_impl=$(TOOLSET)
+endif
+
 ifdef TOOLSET
-include $(MAKEFOO_dir)/native-$(TOOLSET).mk
+include $(MAKEFOO_dir)/native-$(TOOLSET_impl).mk
 endif
 
 # jedit: :tabSize=8:mode=makefile:
